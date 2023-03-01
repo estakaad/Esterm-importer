6cc0702931ce281875382afca317ca004e7f61b8
Esta Prangel <esta.prangel@gmail.com>
2023-03-01 21:23:39 +0200

    Spaghetti code to methods

--- parse.py
+++ parse.py
@@ -2,49 +2,58 @@ import xml.etree.ElementTree as ET
 import json
 
 
+#Find words and their languages of single concept
+def extract_words(root):
+  for term in root.findall(".//languageGrp"):
+    #Returns list of words with values and languages
+    for lang in term.findall(".//language"):
+      if lang.attrib["type"] == "fr":
+        lang_name="fra"
+      if lang.attrib["type"] == "en":
+        lang_name="eng"
+      if lang.attrib["type"] == "et":
+        lang_name="est"
+
+    for term in term.findall(".//term"):
+      word = {"value": term.text, "lang": lang_name}
+      words.append(word)
+  return words
+
+
+#Find definitions and their languages of single concept
+def extract_definitions(root):
+  for elem in root.findall(".//*[@type]"):
+    if elem.attrib["type"] == "Definitsioon":
+      definition_word = elem.text
+      for elem in root.findall(".//*[@type]"):
+        if elem.attrib["type"] == "et":
+          definition = {"value": definition_word, "lang": "est", "definitionTypeCode": "definitsioon" }
+      definitions.append(definition)
+      return definitions
+
+
+#Create dictionary which contains a list of definitions and a list of words
+def create_concept(dataset_code, definitions, words):
+  concept = {
+    "datasetCode": dataset_code,
+    "definitions": definitions,
+    "words": words
+    }
+  return concept
+
+
 tree = ET.parse("test.xml")
 root = tree.getroot()
 
-lang_name=""
 concept = {}
-datasetCode = "mlt"
+dataset_code = "mlt"
 words = []
 definitions = []
 
 
-#Find words and their languages
-for term in root.findall(".//languageGrp"):
-  #Returns list of words with values and languages
-  for lang in term.findall(".//language"):
-    if lang.attrib["type"] == "fr":
-      lang_name="fra"
-    if lang.attrib["type"] == "en":
-      lang_name="eng"
-    if lang.attrib["type"] == "et":
-      lang_name="est"
+words = extract_words(root)
+definitions = extract_definitions(root)
 
-  for term in term.findall(".//term"):
-    word = {"value": term.text, "lang": lang_name}
-    words.append(word)
-
-
-#Find definitions and their languages
-for elem in root.findall(".//*[@type]"):
-  if elem.attrib["type"] == "Definitsioon":
-    definition_word = elem.text
-    for elem in root.findall(".//*[@type]"):
-      if elem.attrib["type"] == "et":
-        definition = {"value": definition_word, "lang": "est", "definitionTypeCode": "definitsioon" }
-
-    definitions.append(definition)
-    print(definition)
-
-
-concept = {
-  "datasetCode": datasetCode,
-  "definitions": definitions,
-  "words": words
-  }
-
+concept = create_concept(dataset_code, definitions, words)
 
 print(concept)
