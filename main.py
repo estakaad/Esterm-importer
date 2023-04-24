40a32754b0778875d3c065cd74bfa8113e403f85
Esta Prangel <esta.prangel@eki.ee>
2023-04-24 13:25:42 +0300

    ElementTree didn parse XML with UTF-16 encoding

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import parse
 import xml.etree.ElementTree as ET
 import json
 import api_requests
-from dotenv.main import load_dotenv
+import os
+from dotenv import load_dotenv
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s', filename="import")
 
-tree = ET.parse("test.xml")
+parser = ET.XMLParser(encoding="UTF-8")
+
+tree = ET.parse("test.xml", parser=parser)
 root = tree.getroot()
 
 dataset_code = "mlt"
@@ -16,7 +19,7 @@ dataset_code = "mlt"
 concepts = parse.extract_concepts(root, "mlt")
 
 load_dotenv()
-api_key=os.environ.get('API_KEY')
+api_key=os.environ.get("API_KEY")
 
 headers = {"ekilex-api-key": api_key}
 parameters = {"crudRoleDataset": "mlt"}
