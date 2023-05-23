5cf58f41648b556b76aead679cdc03fdfce05474
Esta Prangel <esta.prangel@eki.ee>
2023-05-23 10:09:17 +0300

    Refactoring

--- parse_esterm.py
+++ parse_esterm.py
@@ -37,7 +37,7 @@ class Word:
     value: str
     lang: str
     is_public: int
-    word_type: str
+    word_type: Optional[str] = None
     value_state_code: Optional[str] = None
     usage: List[str] = field(default_factory=list)
     notes: List[str] = field(default_factory=list)
@@ -96,60 +96,10 @@ def parse_mtf(root):
                     is_public=1
                 ))
 
-        for languageGrp in conceptGrp.xpath('languageGrp'):
-            lang = languageGrp.xpath('language')[0].get('lang') if languageGrp.xpath('language') else 'testing'
 
-            termGrps = languageGrp.xpath('termGrp')
+        word = parse_words(conceptGrp, concept)
 
-            for termGrp in termGrps:
-                term = termGrp.xpath('term')[0].text if termGrp.xpath('term') else 'testing'
-                if termGrp.xpath('descripGrp/descrip[@type="Keelenditüüp"]'):
-                    term_text = termGrp.xpath('descripGrp/descrip[@type="Keelenditüüp"]')[0].text
-                    value_state_code = None
-                    if term_text == 'eelistermin' and len(termGrps) > 1:
-                        value_state_code = 'eelistatud'
-                    elif term_text == 'sünonüüm':
-                        value_state_code = 'mööndav'
-                    elif term_text == 'variant':
-                        value_state_code = 'variant - seda ignoreerime'
-                    elif term_text == 'endine':
-                        value_state_code = 'endine'
-                    elif term_text == 'väldi':
-                        value_state_code = 'väldi'
-                else:
-                    value_state_code = 'testing - vist pole väärtust'
-
-                word = Word(
-                    value=term,
-                    lang=xml_helpers.match_language(lang),
-                    is_public=1,
-                    word_type='testing',
-                    value_state_code=value_state_code
-                )
-
-                for descripGrp in termGrp.xpath('descripGrp'):
-                    descrip_type = descripGrp.xpath('descrip/@type')[0]
-                    descrip_text = descripGrp.xpath('descrip')[0].text
-                    if descrip_type == 'Märkus':
-                        word.notes.append(descrip_text)
-                    elif descrip_type == 'Kontekst':
-                        word.usage.append(descrip_text)
-                    elif descrip_type == 'Allikas':
-                        print('allikas')
-                    elif descrip_type == 'Definitsioon':
-                        definition_text = descrip_text.strip() if descrip_text is not None else 'testing'
-                        if descripGrp.xpath('descrip/xref'):
-                            source = descripGrp.xpath('descrip/xref')[0].text
-                        else:
-                            source = 'testallikas'
-                        concept.definitions.append(Definition(
-                            value=descrip_text.split('[')[0].strip(),
-                            lang=xml_helpers.match_language(lang) if lang is not None else 'testing',
-                            definitionTypeCode='definitsioon',
-                            source=source
-                        ))
-
-                concept.words.append(word)
+        concept.words.append(word)
 
         list_to_append.append(concept)
 
@@ -157,6 +107,98 @@ def parse_mtf(root):
 
     return concepts, sources, aviation_concepts
 
+
+def parse_words(conceptGrp, concept):
+    words = dict()
+
+    for languageGrp in conceptGrp.xpath('languageGrp'):
+        lang = languageGrp.xpath('language')[0].get('lang') if languageGrp.xpath('language') else 'testing'
+
+        termGrps = languageGrp.xpath('termGrp')
+
+        for termGrp in termGrps:
+            term = termGrp.xpath('term')[0].text if termGrp.xpath('term') else 'testing'
+            word_type = None
+            value_state_code = None
+
+            word = Word(
+                value=term,
+                lang=xml_helpers.match_language(lang),
+                is_public=1,
+                word_type=word_type,
+                value_state_code=value_state_code
+            )
+
+            if value_state_code in ["sünonüüm", "endine"]:
+                words[value_state_code] = word
+
+            for descripGrp in termGrp.xpath('descripGrp'):
+                descrip_type = descripGrp.xpath('descrip/@type')[0]
+                descrip_text = descripGrp.xpath('descrip')[0].text
+
+                if descrip_type == 'Märkus':
+                    if descrip_text.startswith(("SÜNONÜÜM: ", "ENDINE: ", "VARIANT: ")):
+                        # Add the note to the words with "mööndav" or "endine" value_state_codes
+                        if "SÜNONÜÜM: " in descrip_text and "mööndav" in words:
+                            words["mööndav"].notes.append(descrip_text)
+                        elif "ENDINE: " in descrip_text and "endine" in words:
+                            words["endine"].notes.append(descrip_text)
+                    else:
+                        word.notes.append(descrip_text)
+
+            if termGrp.xpath('descripGrp/descrip[@type="Keelenditüüp"]'):
+                term_text = termGrp.xpath('descripGrp/descrip[@type="Keelenditüüp"]')[0].text
+                value_state_code = None
+                word_type = None
+                # Kui keelenditüüp on "eelistermin" ja languageGrp element sisaldab rohkem kui üht termGrp elementi,
+                # tuleb Ekilexis väärtusoleku väärtuseks salvestada "eelistatud"
+                if term_text == 'eelistermin' and len(termGrps) > 1:
+                    value_state_code = 'eelistatud'
+                # Kui keelenditüüp on "lühend", tuleb Ekilexis keelenditüübi väärtuseks salvestada "lühend"
+                elif term_text == 'lühend':
+                    word_type = 'l'
+                # Kui keelenditüüp on "sünonüüm" ja termin on kohanimi, tuleb Ekilexis väärtusolekuks
+                # salvestada "mööndav". Kui keelenditüüp on "sünonüüm" ja termin ei ole kohanimi, siis Ekilexis ?
+                elif term_text == 'sünonüüm':
+                    value_state_code = 'mööndav'
+                # Kui keelenditüüp on "variant", siis Ekilexis väärtusolekut ega keelenditüüpi ei salvestata.
+                elif term_text == 'variant':
+                    value_state_code = None
+                # Kui keelenditüüp on "endine", tuleb Ekilexis väärtusoleku väärtuseks salvestada "endine"
+                elif term_text == 'endine':
+                    value_state_code = 'endine'
+                # Kui keelenditüüp on "väldi", tuleb Ekilexis väärtusoleku väärtuseks salvestada "väldi"
+                elif term_text == 'väldi':
+                    value_state_code = 'väldi'
+            else:
+                value_state_code = 'testing - vist pole väärtust'
+
+            for descripGrp in termGrp.xpath('descripGrp'):
+                descrip_type = descripGrp.xpath('descrip/@type')[0]
+                descrip_text = descripGrp.xpath('descrip')[0].text
+                # Kui /mtf/conceptGrp/languageGrp/termGrp/descripGrp/descrip[@type="Märkus"] alguses on
+                # "SÜNONÜÜM: ", "VARIANT: " või "ENDINE: ", siis tuleb see salvestada selle termGrp
+                # elemendi märkuseks, mille keelenditüüp Estermis on "SÜNONÜÜM", "VARIANT" või "ENDINE"
+                if descrip_type == 'Märkus':
+                    word.notes.append(descrip_text)
+                elif descrip_type == 'Kontekst':
+                    word.usage.append(descrip_text)
+                elif descrip_type == 'Allikas':
+                    print('allikas')
+                elif descrip_type == 'Definitsioon':
+                    definition_text = descrip_text.strip() if descrip_text is not None else 'testing'
+                    if descripGrp.xpath('descrip/xref'):
+                        source = descripGrp.xpath('descrip/xref')[0].text
+                    else:
+                        source = 'testallikas'
+                    concept.definitions.append(Definition(
+                        value=descrip_text.split('[')[0].strip(),
+                        lang=xml_helpers.match_language(lang) if lang is not None else 'testing',
+                        definitionTypeCode='definitsioon',
+                        source=source
+                    ))
+
+    return word
 
 def print_concepts_to_json(concepts, sources, aviation_concepts):
     print(str(len(concepts)))
