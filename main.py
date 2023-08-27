ed82145104841af30493c23c8cca539ebce33159
Esta Prangel <esta.prangel@eki.ee>
2023-08-27 13:17:24 +0300

    Edits for testing (do not create new source, use dummy id)

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import parse_concepts
 import parse_sources
 import requests_concepts
 import requests_sources
+import json
+
 
 # First parse XML and find potential expert names. Then create sources for these experts
 #expert_sources = parse_sources.create_expert_sources(parse_sources.export_experts_names_from_xml('files/input/esterm.xml'))
@@ -12,11 +14,15 @@ import requests_sources
 # 2. Get ID-s of existing sources. If source doesn't exist yet, create it and get its ID.
 # Add the sources with their ID-s to file output/sources_with_ids.json
 # Add ID-s of created sources to files/output/ids_of_created_sources.json
+# Return file output/sources_with_ids.json
 
-#updated_sources = requests_sources.assign_ids_to_all_sources('files/output/sources-test.json')
+#updated_sources_file = requests_sources.assign_ids_to_all_sources('files/output/sources.json')
 
 # 3. Export concepts from XML. Returns files/output/concepts.json and files/output/aviation_concepts.json
-#parse_concepts.transform_esterm_to_json(updated_sources)
+with open('files/output/sources_with_ids-backup.json', 'r', encoding='utf-8') as f:
+    updated_sources = json.load(f)
+
+parse_concepts.transform_esterm_to_json(updated_sources)
 
 # 4. Import all concepts from file files/output/concepts.json or files/output/aviation_concepts.json.
 # List of ID-s of concepts is saved to files/output/
